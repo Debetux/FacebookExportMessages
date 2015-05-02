8f482aaa511226f358eb619dfaccd6f5859485ac
Lancelot HARDEL <debetux@gmail.com>
2015-05-02 16:34:15 +0200

    Mandrill configuration

--- tasks.py
+++ tasks.py
@@ -1,12 +1,14 @@
 from celery import Celery
 from settings import CELERY_RESULT_BACKEND, BROKER_URL
+import mandrill
 
 app = Celery('example')
-
 app.conf.update(BROKER_URL=BROKER_URL,
                 CELERY_RESULT_BACKEND=CELERY_RESULT_BACKEND,
                 CELERY_TASK_SERIALIZER='json',
                 CELERY_ACCEPT_CONTENT=['json'])
+
+mandrill_client = mandrill.Mandrill(MANDRILL_APIKEY)
 
 
 @app.task
