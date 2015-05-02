deaebbdfa51862e5c1395a59e062833cb5235b54
Lancelot HARDEL <debetux@gmail.com>
2015-05-02 15:43:29 +0200

    Add new settings

--- settings.py
+++ settings.py
@@ -3,3 +3,5 @@ import os
 FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
 FACEBOOK_APP_SECRET = os.environ.get('FACEBOOK_APP_SECRET')
 SECRET_KEY = os.environ.get('SECRET_KEY')
+BROKER_URL = os.environ.get('BROKER_URL')
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND')
