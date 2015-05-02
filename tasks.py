fe4a80b3f22e56e270c97d730da3ba5be939eed8
Lancelot HARDEL <debetux@gmail.com>
2015-05-02 16:19:44 +0200

    Celery update

--- tasks.py
+++ tasks.py
@@ -4,7 +4,8 @@ from settings import CELERY_RESULT_BACKEND, BROKER_URL
 app = Celery('example')
 
 app.conf.update(BROKER_URL=BROKER_URL,
-                CELERY_RESULT_BACKEND=CELERY_RESULT_BACKEND)
+                CELERY_RESULT_BACKEND=CELERY_RESULT_BACKEND,
+                CELERY_TASK_SERIALIZER='json')
 
 
 @app.task
