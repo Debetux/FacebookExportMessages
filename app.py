14aca6a198feeb5e89674689c5884cbbfd1608b9
Lancelot HARDEL <debetux@gmail.com>
2015-05-26 21:45:31 +0200

    DATABASE, config and ALLTHETHINGS

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 import os
+import urllib.parse
 
 from settings import *
-from peewee import SqliteDatabase
+from peewee import PostgresqlDatabase
 
 APP_ROOT = os.path.dirname(os.path.realpath(__file__))
-DATABASE = os.path.join(APP_ROOT, 'app.db')
+urllib.parse.uses_netloc.append('postgres')
+DATABASE = urllib.parse.urlparse(DATABASE_URL)
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-db = SqliteDatabase(app.config['DATABASE'], threadlocals=True)
+db = PostgresqlDatabase(DATABASE.path[1:], user=DATABASE.username, host=DATABASE.hostname, port=DATABASE.port)
--- settings.py
+++ settings.py
@@ -1,4 +1,16 @@
 import os
+import glob
+import sys
+
+if 'test' in sys.argv:
+    env_dir = os.path.join('tests', 'envdir')
+else:
+    env_dir = 'envdir'
+env_vars = glob.glob(os.path.join(env_dir, '*'))
+for env_var in env_vars:
+    with open(env_var, 'r') as env_var_file:
+        os.environ.setdefault(env_var.split(os.sep)[-1],
+                              env_var_file.read().strip())
 
 FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
 FACEBOOK_APP_SECRET = os.environ.get('FACEBOOK_APP_SECRET')
@@ -7,3 +19,4 @@ BROKER_URL = os.environ.get('BROKER_URL')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND')
 MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
 MAILGUN_SENDER = os.environ.get('MAILGUN_SENDER')
+DATABASE_URL = os.environ.get('DATABASE_URL')
